59c8aaba5b23c6acc13f10bd86450be3e624f525
Тимофей Сорокин <timabananaman@gmail.com>
2024-02-17 18:29:48 +0300

    Ready

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import {Open_Sans} from "next/font/google";
 import "./globals.css";
+import {ClerkProvider} from "@clerk/nextjs";
 
 const font = Open_Sans({subsets: ["latin"]});
 
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: 'build',
+    output: 'export',
 };
 export default nextConfig;
 
