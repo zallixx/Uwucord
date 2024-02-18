5a995c09eaaa8ae6392e76ed13c455855a2e5fb9
Тимофей Сорокин <timabananaman@gmail.com>
2024-02-18 22:35:25 +0300

    theme support

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import {UserButton} from "@clerk/nextjs";
+import {ModeToggle} from "@/components/mode-toggle";
 
 export default function Home() {
   return (
@@ -6,6 +8,8 @@ export default function Home() {
           <p className="text-3xl font-bold text-indigo-500">
               Discord Test
         </p>
+          <UserButton afterSignOutUrl="/"/>
+          <ModeToggle/>
       </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import {Open_Sans} from "next/font/google";
 import './globals.css'
 import {ClerkProvider} from "@clerk/nextjs";
 
+import {ThemeProvider} from "@/components/providers/theme-provider";
+import {cn} from "@/lib/utils";
+
 const font = Open_Sans({subsets: ["latin"]});
 
 export const metadata: Metadata = {
@@ -13,8 +16,15 @@ export const metadata: Metadata = {
 export default function RootLayout({children,}: Readonly<{ children: React.ReactNode; }>) {
     return (
         <ClerkProvider>
-            <html lang="en">
-            <body className={font.className}>{children}</body>
+            <html lang="en" suppressHydrationWarning>
+            <body className={cn(
+                font.className,
+                "bg-white dark:bg-[#313338]"
+            )}>
+            <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} storageKey="discord-theme">
+                {children}
+            </ThemeProvider>
+            </body>
             </html>
         </ClerkProvider>
     );
