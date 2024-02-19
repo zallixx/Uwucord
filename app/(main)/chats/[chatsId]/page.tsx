57abf94313eb0b561988cf52620c75d9e43585fe
Алексей Морозов <almoroz08@gmail.com>
2024-02-19 20:51:11 +0300

    макет для страницы с личными чатами

--- app/(main)/chats/[chatsId]/page.tsx
+++ app/(main)/chats/[chatsId]/page.tsx
@@ -1,8 +1,19 @@
+import {ScrollArea} from "@/components/ui/scroll-area";
+import {Separator} from "@/components/ui/separator";
+import {Button} from "@/components/ui/button";
+import {UsersRound} from "lucide-react";
+
 const ChatsIdPage = () => {
     return (
-        <div>
-            List of chats
-        </div>
+        <>
+            <div className="flex h-screen">
+                <div className="w-52 bg-[#2b2d31]">1</div>
+                <div className="flex-1 flex flex-col">
+                    <div className="h-12">2</div>
+                    <div className="flex-1">3</div>
+                </div>
+            </div>
+        </>
     );
 }
 
