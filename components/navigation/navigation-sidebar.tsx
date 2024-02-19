01b67a98194013c418f1df526def8dcbba0b00d1
Алексей Морозов <almoroz08@gmail.com>
2024-02-19 19:37:40 +0300

    обновил сайдбар

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -3,6 +3,7 @@ import {currentProfile} from "@/lib/current-profile";
 import {redirect} from "next/navigation";
 import {db} from "@/lib/db";
 import {NavigationAction} from "@/components/navigation/navigation-action";
+import {NavigationPrivateMessages} from "@/components/navigation/navigation-private-messages";
 import {Separator} from "@/components/ui/separator";
 import {ScrollArea} from "@/components/ui/scroll-area";
 import {NavigationItem} from "@/components/navigation/navigation-item";
@@ -28,7 +29,7 @@ export const NavigationSidebar = async () => {
 
     return (
         <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
-            <NavigationAction/>
+            <NavigationPrivateMessages/>
             <Separator
                 className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto"
             />
@@ -42,6 +43,7 @@ export const NavigationSidebar = async () => {
                         />
                     </div>
                 ))}
+            <NavigationAction/>
             </ScrollArea>
             <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
                 <ModeToggle/>
