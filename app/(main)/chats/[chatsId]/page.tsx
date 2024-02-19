6ac58e8567cdcf26905b7f806eff02076b7d7e52
Алексей Морозов <almoroz08@gmail.com>
2024-02-19 20:10:54 +0300

    обновил сайдбар

---
+++ app/(main)/chats/[chatsId]/page.tsx
@@ -0,0 +1,9 @@
+const ChatsIdPage = () => {
+    return (
+        <div>
+            List of chats
+        </div>
+    );
+}
+
+export default ChatsIdPage;
--- components/navigation/navigation-item.tsx
+++ components/navigation/navigation-item.tsx
@@ -31,9 +31,9 @@ export const NavigationItem = ({id, imageUrl, name}: NavigationItemProps) => {
                 className='group relative flex items-center'
             >
                 <div className={cn(
-                    "absolute left-0 bg-primary rounded-r-full transition-all w-[4px]",
-                    params?.serverId !== id && "group-hover:h-[20px]",
-                    params?.serverId === id ? "h-[36px]" : "h-[8px]"
+                    "absolute left-0 bg-primary rounded-r-full transition-all w-[0px]",
+                    params?.serverId !== id && "group-hover:h-[20px] group-hover:w-[4px]",
+                    params?.serverId === id ? "h-[36px] w-[4px]" : "h-[8px] group-hover:w-[4px]"
                 )}/>
                 <div
                     className={cn(
