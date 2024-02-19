8c436206fdfc8552003dc19c199d75e995ceed25
Алексей Морозов <almoroz08@gmail.com>
2024-02-19 20:57:31 +0300

    поменял размер левого бара

--- app/(main)/chats/[chatsId]/page.tsx
+++ app/(main)/chats/[chatsId]/page.tsx
@@ -2,12 +2,15 @@ import {ScrollArea} from "@/components/ui/scroll-area";
 import {Separator} from "@/components/ui/separator";
 import {Button} from "@/components/ui/button";
 import {UsersRound} from "lucide-react";
+import {Input} from "@/components/ui/input";
 
 const ChatsIdPage = () => {
     return (
         <>
             <div className="flex h-screen">
-                <div className="w-52 bg-[#2b2d31]">1</div>
+                <div className="w-60 bg-[#2b2d31]">
+                    <Button className="w-full h-7"/>
+                </div>
                 <div className="flex-1 flex flex-col">
                     <div className="h-12">2</div>
                     <div className="flex-1">3</div>
