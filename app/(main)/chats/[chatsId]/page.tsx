1ad13c00bafcf472d9b86b8bbb9705dd1f703681
Алексей Морозов <almoroz08@gmail.com>
2024-02-19 21:56:40 +0300

    новые эл-ты на странице с лс

--- app/(main)/chats/[chatsId]/page.tsx
+++ app/(main)/chats/[chatsId]/page.tsx
@@ -9,10 +9,21 @@ const ChatsIdPage = () => {
         <>
             <div className="flex h-screen">
                 <div className="w-60 bg-[#2b2d31]">
-                    <Button className="w-full h-7"/>
+                    <button className="h-7 mx-4 my-2 px-4 text-[#949ba4] rounded-md bg-[#1e1f22] text-sm ">
+                        Найти или начать беседу
+                    </button>
+                    <Separator/>
                 </div>
                 <div className="flex-1 flex flex-col">
-                    <div className="h-12">2</div>
+                    <div className="h-12">
+                        <div className="my-2">
+                            <p className="flex flex-row mx-1 text-[#f2f3f5]">
+                                <UsersRound color="#80848e" className="mx-2"/>
+                                Друзья
+                            </p>
+                            <Separator orientation="vertical" decorative/>
+                        </div>
+                    </div>
                     <div className="flex-1">3</div>
                 </div>
             </div>
