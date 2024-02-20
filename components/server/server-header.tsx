b41540f33891592b2814f3fa6c39647467aa35d5
Тимофей Сорокин <timabananaman@gmail.com>
2024-02-20 21:33:02 +0300

    more changes

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -24,7 +24,7 @@ import {useModal} from "@/hooks/use-modal-store";
 interface ServerHeaderProps {
     server: ServerWithMembersWithProfiles;
     role?: MemberRole;
-};
+}
 
 export const ServerHeader = ({
                                  server,
@@ -49,53 +49,55 @@ export const ServerHeader = ({
                 </button>
             </DropdownMenuTrigger>
             <DropdownMenuContent
-                className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]"
+                className="w-56  text-[10px] font-normal text-black dark:text-neutral-400 space-y-[2px] bg-zinc-950/80"
             >
                 {isModerator && (
                     <DropdownMenuItem
                         onClick={() => onOpen("invite", {server})}
-                        className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer"
+                        className="text-indigo-600 dark:text-indigo-400 px-3 py-2 cursor-pointer "
                     >
-                        Invite People
+                        Пригласить людей
                         <UserPlus className="h-4 w-4 ml-auto"/>
                     </DropdownMenuItem>
                 )}
                 {isAdmin && (
                     <DropdownMenuItem
                         onClick={() => onOpen("editServer", {server})}
-                        className="px-3 py-2 text-sm cursor-pointer"
+                        className="px-3 py-2 cursor-pointer"
                     >
-                        Server Settings
+                        Настройки сервера
                         <Settings className="h-4 w-4 ml-auto"/>
                     </DropdownMenuItem>
                 )}
                 {isAdmin && (
                     <DropdownMenuItem
                         onClick={() => onOpen("members", {server})}
-                        className="px-3 py-2 text-sm cursor-pointer"
+                        className="px-3 py-2 cursor-pointer"
                     >
-                        Manage Members
+                        Участники
                         <Users className="h-4 w-4 ml-auto"/>
                     </DropdownMenuItem>
                 )}
                 {isModerator && (
                     <DropdownMenuItem
                         onClick={() => onOpen("createChannel")}
-                        className="px-3 py-2 text-sm cursor-pointer"
+                        className="px-3 py-2 cursor-pointer"
                     >
-                        Create Channel
+                        Создать канал
                         <PlusCircle className="h-4 w-4 ml-auto"/>
                     </DropdownMenuItem>
                 )}
                 {isModerator && (
-                    <DropdownMenuSeparator/>
+                    <DropdownMenuSeparator
+                        className="h-[1px] bg-zinc-300 dark:bg-zinc-800/90 rounded-md mx-2"
+                    />
                 )}
                 {isAdmin && (
                     <DropdownMenuItem
                         onClick={() => onOpen("deleteServer", {server})}
-                        className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
+                        className="text-rose-500 px-3 py-2 cursor-pointer"
                     >
-                        Delete Server
+                        Удалить сервер
                         <Trash className="h-4 w-4 ml-auto"/>
                     </DropdownMenuItem>
                 )}
@@ -104,7 +106,7 @@ export const ServerHeader = ({
                         onClick={() => onOpen("leaveServer", {server})}
                         className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
                     >
-                        Leave Server
+                        Покинуть сервер
                         <LogOut className="h-4 w-4 ml-auto"/>
                     </DropdownMenuItem>
                 )}
