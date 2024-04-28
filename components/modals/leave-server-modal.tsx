361d2dc66bdf3ff70b55dbf19c88253713b93e7b
Алексей Морозов <almoroz08@gmail.com>
2024-04-28 22:44:06 +0300

    решил проблему(sonarqube) в components/modals/leave-server-modal.tsx

--- components/modals/leave-server-modal.tsx
+++ components/modals/leave-server-modal.tsx
@@ -48,10 +48,9 @@ function LeaveServerModal() {
                         Покинуть сервер
                     </DialogTitle>
                     <DialogDescription className="text-center text-zinc-500">
-                        Вы уверен, что хотите покинуть
-                        {' '}
-                        <span className="font-semibold text-indigo-500">{server?.name}</span>
-                        ?
+                        Вы уверены, что хотите покинуть{' '}
+                        <span className="font-semibold text-indigo-500">{server?.name}</span>{/*
+                        */}?
                     </DialogDescription>
                 </DialogHeader>
                 <DialogFooter className="bg-gray-100 px-6 py-4">
