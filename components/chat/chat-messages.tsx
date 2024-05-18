380874d21b4f2ab379a13328b69d16aba12a1d94
Тимофей Сорокин <timabananaman@gmail.com>
2024-05-18 14:51:29 +0300

    Member -> Profile

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -10,6 +10,7 @@ import ChatItem from './chat-item';
 import useChatQuery from '@/hooks/use-chat-query';
 import useChatSocket from '@/hooks/use-chat-socket';
 import useChatScroll from '@/hooks/use-chat-scroll';
+import ChatItemDm from '@/components/dm/dm-chat-item';
 
 const DATE_FORMAT = 'd MMM yyyy, HH:mm';
 
@@ -18,10 +19,14 @@ type MessageWithMemberWithProfile = Message & {
         profile: Profile;
     };
 };
+type MessageWithProfile = Message & {
+    profile: Profile;
+};
 
 interface ChatMessagesProps {
     readonly name: string;
-    readonly member: Member;
+    readonly member?: Member;
+    readonly profile?: Profile;
     readonly chatId: string;
     readonly apiUrl: string;
     readonly socketUrl: string;
@@ -32,16 +37,17 @@ interface ChatMessagesProps {
 }
 
 function ChatMessages({
-    name,
-    member,
-    chatId,
-    apiUrl,
-    socketUrl,
-    socketQuery,
-    paramKey,
-    paramValue,
-    type,
-}: ChatMessagesProps) {
+                          name,
+                          member,
+                          profile,
+                          chatId,
+                          apiUrl,
+                          socketUrl,
+                          socketQuery,
+                          paramKey,
+                          paramValue,
+                          type
+                      }: ChatMessagesProps) {
     const queryKey = `chat:${chatId}`;
     const addKey = `chat:${chatId}:messages`;
     const updateKey = `chat:${chatId}:messages:update`;
@@ -54,7 +60,7 @@ function ChatMessages({
             queryKey,
             apiUrl,
             paramKey,
-            paramValue,
+            paramValue
         });
     useChatSocket({ queryKey, addKey, updateKey });
     useChatScroll({
@@ -62,7 +68,7 @@ function ChatMessages({
         bottomRef,
         loadMore: fetchNextPage,
         shouldLoadMore: !isFetchingNextPage && hasNextPage,
-        count: data?.pages?.[0]?.items?.length ?? 0,
+        count: data?.pages?.[0]?.items?.length ?? 0
     });
 
     if (status === 'pending') {
@@ -109,35 +115,68 @@ function ChatMessages({
                     )}
                 </div>
             )}
-            <div className="flex flex-col-reverse mt-auto">
-                {data?.pages?.map((group, id) => (
-                    // eslint-disable-next-line react/no-array-index-key
-                    <Fragment key={id}>
-                        {group.items.map(
-                            (message: MessageWithMemberWithProfile) => (
-                                <ChatItem
-                                    key={message.id}
-                                    id={message.id}
-                                    currentMember={member}
-                                    member={message.member}
-                                    content={message.content}
-                                    fileUrl={message.fileUrl}
-                                    deleted={message.deleted}
-                                    timestamp={format(
-                                        new Date(message.createdAt),
-                                        DATE_FORMAT,
-                                    )}
-                                    isUpdated={
-                                        message.updatedAt !== message.createdAt
-                                    }
-                                    socketUrl={socketUrl}
-                                    socketQuery={socketQuery}
-                                />
-                            ),
-                        )}
-                    </Fragment>
-                ))}
-            </div>
+            {type === 'channel' && (
+                <div className="flex flex-col-reverse mt-auto">
+                    {data?.pages?.map((group, id) => (
+                        // eslint-disable-next-line react/no-array-index-key
+                        <Fragment key={id}>
+                            {group.items.map(
+                                (message: MessageWithMemberWithProfile) => (
+                                    <ChatItem
+                                        key={message.id}
+                                        id={message.id}
+                                        currentMember={member}
+                                        member={message.member}
+                                        content={message.content}
+                                        fileUrl={message.fileUrl}
+                                        deleted={message.deleted}
+                                        timestamp={format(
+                                            new Date(message.createdAt),
+                                            DATE_FORMAT
+                                        )}
+                                        isUpdated={
+                                            message.updatedAt !== message.createdAt
+                                        }
+                                        socketUrl={socketUrl}
+                                        socketQuery={socketQuery}
+                                    />
+                                )
+                            )}
+                        </Fragment>
+                    ))}
+                </div>
+            )}
+            {type === 'conversation' && (
+                <div className="flex flex-col-reverse mt-auto">
+                    {data?.pages?.map((group, id) => (
+                        // eslint-disable-next-line react/no-array-index-key
+                        <Fragment key={id}>
+                            {group.items.map(
+                                (message: MessageWithProfile) => (
+                                    <ChatItemDm
+                                        key={message.id}
+                                        id={message.id}
+                                        currentProfile={profile}
+                                        profile={message.profile}
+                                        content={message.content}
+                                        fileUrl={message.fileUrl}
+                                        deleted={message.deleted}
+                                        timestamp={format(
+                                            new Date(message.createdAt),
+                                            DATE_FORMAT
+                                        )}
+                                        isUpdated={
+                                            message.updatedAt !== message.createdAt
+                                        }
+                                        socketUrl={socketUrl}
+                                        socketQuery={socketQuery}
+                                    />
+                                )
+                            )}
+                        </Fragment>
+                    ))}
+                </div>
+            )}
             <div ref={bottomRef} />
         </div>
     );
