82fcc8baac181397f00b5bddb6a90101ce6e7639
Алексей Морозов <almoroz08@gmail.com>
2024-05-19 19:12:54 +0300

    настраиваю стиль для эл-тов

--- components/dm/dm-friends-item.tsx
+++ components/dm/dm-friends-item.tsx
@@ -16,7 +16,7 @@ function DmFriendsItem({anotherProfile, conversation}: Props) {
     return (
         <>
             <Button key={anotherProfile.id}
-                    className={`h-[61px] dark:text-white dark:bg-transparent dark:hover:bg-[#393c41] border-t border-y dark:border-[#3f4147] rounded-md flex items-center justify-start bg-transparent hover:bg-[#eaebed] text-black border-[#e1e2e4]`}
+                    className={`h-[61px] dark:text-white dark:bg-transparent dark:hover:bg-[#393c41] border-t dark:border-[#3f4147] rounded flex items-center justify-start bg-transparent hover:bg-[#eaebed] text-black border-[#e1e2e4] group`}
                     onClick={() => router.push(`/chats/${conversation?.id}`)}
             >
                 <img
@@ -27,8 +27,8 @@ function DmFriendsItem({anotherProfile, conversation}: Props) {
                 <p className="text-sm">{anotherProfile.name}</p>
                 <div className="ml-auto">
                     <div
-                        className="bg-gray-500 text-white rounded-full w-8 h-8 flex items-center justify-center cursor-pointer">
-                        <MessageCircle className="w-4 h-4"/>
+                        className="text-white rounded-full w-9 h-9 flex items-center justify-center cursor-pointer dark:group-hover:bg-[#1e1f22] dark:bg-[#2b2d31] bg-[#f2f3f5]">
+                        <MessageCircle className="w-[19px] h-[19px] dark:fill-[#b5bac1] fill-[#4e5058] dark:text-[#b5bac1] text-[#4e5058]"/>
                     </div>
                 </div>
             </Button>
